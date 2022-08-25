7d5479d80858123a488f6e95f93c45e009720bff
anish-rm <anishmahi946@gmail.com>
2022-08-25 09:34:13 +0530

    local storage added

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ const display = document.querySelector('.rows');
 const warn = document.querySelector('.warn');
 const id1 = document.querySelector('.idd');
 let i=1;
-const updateUI = ((title,author,id)=>{
-
-    
+let j=0;
+if(localStorage.getItem('title')){
+    j=details.length;
+}
+let details=[];
+const updateAof = ((title1,author1,id1) =>{
+    let temp=[{title : title1,author : author1,id : id1}];
+    details[j]=temp[0];
+    localStorage.setItem('details',JSON.stringify(details));
+    j=j+1;
+});
+const updateUI = ((i,title,author,id)=>{
+    let id1= parseInt(id);
     if(i%2!=0){
         const html = `
         <tr class="disp">
         <th scope="row" class="id${i}">${i}</th>
         <th scope="row">${title}</th>
         <th scope="row">${author}</th>    
-        <th scope="row" class="d-flex justify-content-between align-items-center">${id} <i class="far fa-trash-alt delete"></i></th>        
+        <th scope="row" class="d-flex justify-content-between align-items-center">${id1+1} <i class="far fa-trash-alt delete"></i></th>        
         </tr>`;
         display.innerHTML+=html;
     }
@@ -23,11 +33,11 @@ const updateUI = ((title,author,id)=>{
         <th scope="row">${i}</th>
         <th scope="row">${title}</th>
         <th scope="row">${author}</th>    
-        <th scope="row" class="d-flex justify-content-between align-items-center">${id} <i class="far fa-trash-alt delete"></i></th>        
+        <th scope="row" class="d-flex justify-content-between align-items-center">${id1+1} <i class="far fa-trash-alt delete"></i></th>        
         </tr>`;
         display.innerHTML+=html;
     }
-    i=i+1;
+    id1=id1+1;
 });
 
 const warning = (()=>{
@@ -47,7 +57,11 @@ form.addEventListener('submit',e=>{
     else{
     warn.classList.add('d-none');
     id1.classList.remove('wborder');
-    updateUI(title,author,id);
+    localStorage.setItem('title',title);
+    updateUI(i,title,author,id);
+    i=i+1;
+    updateAof(title,author,id);
+    console.log(details.length);
     form.reset()
     }
 });
@@ -58,8 +72,13 @@ table.addEventListener('click',e=>{
     if(e.target.classList.contains('delete')){
         i=i-1;
         e.target.parentElement.parentElement.remove();
-        console.log(e.target.parentElement.parentElement);
     }
 });
 
 
+
+const todos = [
+    {text : 'play football', author : 'anish'},
+    {text : 'see insta', author : 'agal'},
+    {text : 'get the whole world', author : 'aadvik'}
+];
